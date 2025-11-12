bdc5c93c49ac1c66298163cd559c55146f2251f4
NALLD0 <naldon@nycstudents.net>
2025-11-12 14:06:37 -0500

    pt1

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 
+let maximo = 100
+let minimo = 0 
+
+let Random_Number = 0;
+
+function Randomize_Number(min, max) {
+    Random_Number = Math.random() * (max - min) + min;
+}
+Randomize_Number(maximo, minimo);
+
+console.log(Random_Number); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
